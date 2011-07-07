4b6db0d9b6a8f87e7bbdbe6302f0e9bb3ccfa01f
Tuomas Luttinen <Tuomas.Luttinen@hypoport.de>
2011-07-07 15:18:04 +0200

    Import cleanup.

--- src/main/java/de/hypoport/yatwitter/comments/TwitterCommentPage.java
+++ src/main/java/de/hypoport/yatwitter/comments/TwitterCommentPage.java
@@ -6,27 +6,19 @@ import java.util.List;
 import java.util.Locale;
 
 import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.util.convert.IConverter;
 import org.apache.wicket.util.convert.converters.DateConverter;
 import org.apache.wicket.util.time.Duration;
-import org.apache.wicket.validation.validator.StringValidator;
 
 import de.hypoport.yatwitter.comments.TweetContainer.Tweet;
 import de.hypoport.yatwitter.events.AbstractEvent;
 import de.hypoport.yatwitter.events.IEventListener;
 import de.hypoport.yatwitter.events.NewTweetEvent;
-import de.hypoport.yatwitter.login.LoginLink;
-import de.hypoport.yatwitter.login.LogoutLink;
 import de.hypoport.yatwitter.login.sessions.OnlyWithLogin;
-import de.hypoport.yatwitter.login.sessions.TwitterSession;
 import de.hypoport.yatwitter.pages.AbstractBasePage;
 
 @OnlyWithLogin
