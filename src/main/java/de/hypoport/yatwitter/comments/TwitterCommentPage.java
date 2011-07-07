c598be8bd158e03ff3cd45c2d477849cebf40df2
Michael Mosmann <michael@mosmann.de>
2011-07-07 14:41:03 +0200

    ajax

--- src/main/java/de/hypoport/yatwitter/comments/TwitterCommentPage.java
+++ src/main/java/de/hypoport/yatwitter/comments/TwitterCommentPage.java
@@ -1,16 +1,28 @@
 package de.hypoport.yatwitter.comments;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.convert.converters.DateConverter;
+import org.apache.wicket.util.time.Duration;
 import org.apache.wicket.validation.validator.StringValidator;
 
 import de.hypoport.yatwitter.comments.TweetContainer.Tweet;
+import de.hypoport.yatwitter.events.AbstractEvent;
+import de.hypoport.yatwitter.events.IEventListener;
+import de.hypoport.yatwitter.events.NewTweetEvent;
 import de.hypoport.yatwitter.login.LoginLink;
 import de.hypoport.yatwitter.login.LogoutLink;
 import de.hypoport.yatwitter.login.sessions.OnlyWithLogin;
@@ -18,38 +30,47 @@ import de.hypoport.yatwitter.login.sessions.TwitterSession;
 import de.hypoport.yatwitter.pages.AbstractBasePage;
 
 @OnlyWithLogin
-public class TwitterCommentPage extends AbstractBasePage {
+public class TwitterCommentPage extends AbstractBasePage implements IEventListener {
 
 	private Form<String> commentForm;
+	private Label dateLabel;
 
 	public TwitterCommentPage() {
 		
 		setTitle("Eigene Tweets");
 		setHeadline("Was machst Du?");
 		
-		final Model<String> commentAreaModel = Model.of("");
-		commentForm = new Form<String>("commentFormId") {
-
+		add(new NewTweetPanel("newtweets"));
+		add(new TweetsPanel("tweets", (IModel<? extends List<? extends Tweet>>) new TweetsModel()));
+		
+		IModel<Date> dateModel=new LoadableDetachableModel<Date>() {
 			@Override
-			protected void onSubmit() {
-				final String comment = commentAreaModel.getObject();
-				final TwitterSession session = (TwitterSession) getSession();
-				final String username = session.getLoggedUsername();
-
-				TweetContainer.addTweet(username, comment);
-//				setResponsePage(TweetsPage.class);
+			protected Date load() {
+				return new Date();
 			}
 		};
+		dateLabel = new Label("datum",dateModel) {
+			@Override
+			public IConverter getConverter(Class<?> type) {
+				return new DateConverter() {
+					@Override
+					public DateFormat getDateFormat(Locale locale) {
+						return DateFormat.getDateTimeInstance();
+					}
+				};
+			}
+		};
+		dateLabel.setOutputMarkupId(true);
 		
-		TextField<String> textArea = new TextField<String>("textAreaComment", commentAreaModel);
-		textArea.setRequired(true);
-		textArea.add(new StringValidator.MinimumLengthValidator(2));
-		textArea.add(new StringValidator.MaximumLengthValidator(140));
-		commentForm.add(textArea);
-		
-		add(commentForm);
-		
-		add(new TweetsPanel("tweets", (IModel<? extends List<? extends Tweet>>) new TweetsModel()));
+		dateLabel.add(new AjaxSelfUpdatingTimerBehavior(Duration.seconds(5)));
+		add(dateLabel);
 
+	}
+	
+	public void notify(AbstractEvent event) {
+		if (event instanceof NewTweetEvent) {
+			event.update(dateLabel);
+		}
+		
 	}
 }
---
+++ src/main/java/de/hypoport/yatwitter/events/AbstractEvent.java
@@ -0,0 +1,37 @@
+package de.hypoport.yatwitter.events;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+
+
+public class AbstractEvent {
+
+	private final MarkupContainer source;
+	private AjaxRequestTarget ajaxRequestTarget;
+
+	protected AbstractEvent(MarkupContainer source) {
+		this.source = source;
+		this.ajaxRequestTarget = AjaxRequestTarget.get();
+	}
+	
+	public void fire() {
+		source.getPage().visitChildren(IEventListener.class, new Component.IVisitor<Component>() {
+			public Object component(Component component) {
+				IEventListener eventCom=(IEventListener) component;
+				eventCom.notify(AbstractEvent.this);
+				return Component.IVisitor.CONTINUE_TRAVERSAL;
+			}
+		});
+		if (source.getPage() instanceof IEventListener) {
+			IEventListener epage=(IEventListener) source.getPage();
+			epage.notify(AbstractEvent.this);
+		}
+	}
+
+	public void update(Component component) {
+		if (ajaxRequestTarget!=null) {
+			ajaxRequestTarget.addComponent(component);
+		}
+	}
+}
