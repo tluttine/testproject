59cad3c0f7d69701c9b46f61bf195d78c3c241c8
Steffen Kämpke <steffen.kaempke@hypoport.de>
2011-07-12 10:29:08 +0200

    LoginPage und RegisterPage an Datenbank angebunden

--- src/main/java/de/hypoport/yatwitter/register/RegisterPage.java
+++ src/main/java/de/hypoport/yatwitter/register/RegisterPage.java
@@ -2,25 +2,30 @@ package de.hypoport.yatwitter.register;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import de.hypoport.yatwitter.comments.TwitterCommentPage;
 import de.hypoport.yatwitter.login.LoginData;
 import de.hypoport.yatwitter.login.LoginPage;
+import de.hypoport.yatwitter.login.sessions.TwitterSession;
 import de.hypoport.yatwitter.pages.AbstractBasePage;
 import de.hypoport.yatwitter.persistence.LoginDataDao;
 
-public final class RegisterPage extends AbstractBasePage{
-	
-	private Form<RegisterData> registerDataForm; 
-	private IModel<RegisterData> registerFormModel = new CompoundPropertyModel<RegisterData>(new RegisterData());
-	
+public final class RegisterPage extends AbstractBasePage {
+
+	private Form<RegisterData> registerDataForm;
+	private final IModel<RegisterData> registerFormModel = new CompoundPropertyModel<RegisterData>(new RegisterData());
+
 	@SpringBean(name = LoginDataDao.BEAN_ID)
 	private LoginDataDao loginDataDao;
-	
+
 	public RegisterPage() {
 		initializeComponents();
 	}
@@ -38,38 +43,41 @@ public final class RegisterPage extends AbstractBasePage{
 					return;
 				}
 				String password = registerFormModel.getObject().getPassword();
-				loginDataDao.save(new LoginData(username, password));
+				final LoginData loginData = new LoginData(username, password);
+				loginDataDao.save(loginData);
+				TwitterSession.get().setUser(loginData);
+
+				setResponsePage(TwitterCommentPage.class);
 			}
-		
+
 		};
-		
+
 		final TextField<String> textFieldUserName = new TextField<String>("username");
 		textFieldUserName.setRequired(true);
-		
-		final TextField<String> textFieldPassword = new TextField<String>("password");
+
+		final PasswordTextField textFieldPassword = new PasswordTextField("password");
 		textFieldPassword.setRequired(true);
-		
-		final TextField<String> textFieldPasswordRepeat = new TextField<String>("passwordRepeat");
+
+		final PasswordTextField textFieldPasswordRepeat = new PasswordTextField("passwordRepeat");
 		textFieldPasswordRepeat.setRequired(true);
-		
-		registerDataForm.add(new Label("labelUsername",Model.of("Benutzername")));
-		registerDataForm.add(new Label("labelPassword",Model.of("Passwort")));
-		registerDataForm.add(new Label("labelPasswordRepeat",Model.of("Passwort wiederholen")));
-		
+
+		registerDataForm.add(new Label("labelUsername", Model.of("Benutzername")));
+		registerDataForm.add(new Label("labelPassword", Model.of("Passwort")));
+		registerDataForm.add(new Label("labelPasswordRepeat", Model.of("Passwort wiederholen")));
+
 		registerDataForm.add(textFieldUserName);
 		registerDataForm.add(textFieldPassword);
 		registerDataForm.add(textFieldPasswordRepeat);
-		
-		
+
 		add(registerDataForm);
-		add(new Link<String>("linkToLogin",Model.of("zurück zur Anmeldung")) {
+		add(new Link<String>("linkToLogin", Model.of("zurück zur Anmeldung")) {
 
 			@Override
 			public void onClick() {
 				setResponsePage(LoginPage.class);
 			}
 		});
-		
-		
+
+		registerDataForm.add(new EqualPasswordInputValidator(textFieldPassword, textFieldPasswordRepeat));
 	}
 }
